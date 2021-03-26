b81002ee72ef213eebb07afd30e80102102c344b
gael-lejeune <lejeune.gael@free.fr>
2021-03-26 09:05:45 +0100

    Fin TD1

---
+++ app/src/main/java/com/example/projetmobile/presentation/list/PCListFragment.kt
@@ -0,0 +1,49 @@
+package com.example.projetmobile.presentation.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projetmobile.R
+
+class PCListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private val adapter = PCAdapter(listOf())
+    private val layoutManager = LinearLayoutManager(context)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pc_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.pc_recyclerview)
+
+        recyclerView.apply {
+            layoutManager = this@PCListFragment.layoutManager
+            adapter = this@PCListFragment.adapter
+        }
+
+        val pclist = arrayListOf<String>().apply {
+            add("Mario")
+            add("Donkey Kong")
+            add("Link")
+            add("Samus")
+            add("Dark Samus")
+        }
+
+        adapter.updateList(pclist)
+
+    }
+
+}
