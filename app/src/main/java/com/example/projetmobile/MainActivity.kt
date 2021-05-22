d2b58f93b994d1550158fc519f02a657b4ee9f46
gael-lejeune <lejeune.gael@free.fr>
2021-05-22 19:01:46 +0200

    Implemented theme and cache with sharedprefs

--- app/src/main/java/com/example/projetmobile/MainActivity.kt
+++ app/src/main/java/com/example/projetmobile/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.projetmobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.bundleOf
+import androidx.preference.PreferenceManager.getDefaultSharedPreferences
 import com.example.projetmobile.presentation.AboutFragment
 import com.example.projetmobile.presentation.MenuFragment
 import com.example.projetmobile.presentation.detail.PCDetailFragment
@@ -11,13 +17,58 @@ import com.example.projetmobile.presentation.list.PC
 import com.example.projetmobile.presentation.list.PCListFragment
 import com.google.gson.Gson
 
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences;
+    private lateinit var editor: SharedPreferences.Editor
+    private var itheme: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getDefaultSharedPreferences(this) // 0 - for private mode
+        editor = sharedPreferences.edit()
+        itheme = sharedPreferences.getInt("theme", 1)
+        println(itheme)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(itheme)
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, MenuFragment(), "")
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.settings, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.theme_dark -> {
+                Toast.makeText(this, "Dark theme selected", Toast.LENGTH_SHORT).show()
+                //setTheme(R.style.Dark)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putInt("theme", AppCompatDelegate.MODE_NIGHT_YES)
+                editor.commit()
+
+                true
+            }
+            R.id.theme_light -> {
+                Toast.makeText(this, "Light theme selected", Toast.LENGTH_SHORT).show()
+                //setTheme(R.style.Light)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putInt("theme", AppCompatDelegate.MODE_NIGHT_NO)
+                editor.commit()
+
+                true
+            }
+            R.id.language -> {
+                Toast.makeText(this, "Language selected", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
